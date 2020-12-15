8c381b35818a8b3229d60dabbcf525a7b8df3cb5
JHY34 <hamityilmaz34@gmail.com>
2020-12-15 14:43:42 -0600

    added BeforeAll, AfterAll Annotations in the CRUD practice class

--- src/test/java/Z_Practice/CRUD_Practice1.java
+++ src/test/java/Z_Practice/CRUD_Practice1.java
@@ -1,4 +1,28 @@
 package Z_Practice;
 
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utility.ConfigurationReader;
+
+import java.util.List;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class CRUD_Practice1 {
+
+    @BeforeAll
+    public static void setUp() {
+        baseURI = ConfigurationReader.getProperty("spartan.base_url");
+        basePath = "/api";
+    }
+
+    @AfterAll
+    public static void tearDown () {
+        reset();
+    }
+
 }
