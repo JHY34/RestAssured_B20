ad800ea4f3545c3bb8557f3387d7c64e19974499
JHY34 <hamityilmaz34@gmail.com>
2020-12-16 18:29:48 -0600

    added one spartan

--- src/test/java/day07/PatchOneSpartanTest.java
+++ src/test/java/day07/PatchOneSpartanTest.java
@@ -1,20 +1,56 @@
 package day07;
 
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 import io.restassured.http.ContentType;
 import pojo.Spartan;
-import pojo.SpartanRead;
 import utility.ConfigurationReader;
 import utility.SpartanUtil;
 
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.* ;
 import static org.hamcrest.Matchers.* ;
 
+
 public class PatchOneSpartanTest {
+
+    @BeforeAll
+    public static void setUp(){
+        //RestAssured.filters().add(new AllureRestAssured() ) ;
+        baseURI = ConfigurationReader.getProperty("spartan.base_url");
+        basePath = "/api" ;
+    }
+
+
+    @AfterAll
+    public static void tearDown(){
+        reset();
+    }
+
+
+    @DisplayName("Patching 1 data with Java Object")
+    @Test
+    public void testPath1DataPartialUpdate(){
+
+        // we just want to update the name and phone number
+        Map<String , Object> patchBodyMap = new LinkedHashMap<>();
+        patchBodyMap.put("name" , "Osman");
+        patchBodyMap.put("gender" , "Male");
+        patchBodyMap.put("phone" , 5554443332L);
+
+        given()
+                .auth().basic("admin" , "admin")
+                .pathParam("id" , 100)
+                .contentType(ContentType.JSON)
+                .body(patchBodyMap).
+        when()
+                .patch("/spartans/{id}").
+        then()
+                .log().all()
+                .statusCode(is(204))
+
+                ;
+    }
 }
