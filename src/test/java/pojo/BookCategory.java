a37d099292c5627ccc60727a5062f3dd75344e1a
JHY34 <hamityilmaz34@gmail.com>
2020-12-16 19:50:53 -0600

    matched the names

--- src/test/java/pojo/BookCategory.java
+++ src/test/java/pojo/BookCategory.java
@@ -1,24 +1,28 @@
 package pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class BookCategory {
 
+    @JsonProperty ("id")
     private String category_id;
+    @JsonProperty("name")
     private String category_name;
 
     public String getId() {
         return category_id;
     }
 
-    public void setId(String id) {
-        this.category_id = id;
+    public void setId(String category_id) {
+        this.category_id = category_id;
     }
 
     public String getName() {
         return category_name;
     }
 
-    public void setName(String name) {
-        this.category_name = name;
+    public void setName(String category_name) {
+        this.category_name = category_name;
     }
 
 
