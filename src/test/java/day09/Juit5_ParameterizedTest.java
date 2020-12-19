7c20ed453f3a7901fdc1ad9dccea503973c2b59e
JHY34 <hamityilmaz34@gmail.com>
2020-12-19 14:45:22 -0600

    resources folder is created and csv file is added

--- src/test/java/day09/Juit5_ParameterizedTest.java
+++ src/test/java/day09/Juit5_ParameterizedTest.java
@@ -1,8 +1,22 @@
 package day09;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Juit5_ParameterizedTest {
 
-    public void test1 () {
+    @ParameterizedTest  // when you say @ParameterizedTest you need to add source as seen below
+    @ValueSource (ints = {5,6,7,8,9,10})   // each number is injected every test -> 5 test 1, 6 test2 ....
+    public void test1 (int myNumber) {
+
+        System.out.println("myNumber = " + myNumber);
+        // assert 5,6,7,8,9,10  all less than 10
+        assertTrue(myNumber <=100) ;
+        // all PASSED
+
+
 
     }
 
