250f758a2fccd544fc3e4773d2fbf34c7e9ab650
JHY34 <hamityilmaz34@gmail.com>
2020-12-19 15:39:54 -0600

    added 2 csv file under resources and test them

--- src/test/java/day09/Juit5_ParameterizedTest.java
+++ src/test/java/day09/Juit5_ParameterizedTest.java
@@ -1,7 +1,12 @@
 package day09;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,9 +20,48 @@ public class Juit5_ParameterizedTest {
         // assert 5,6,7,8,9,10  all less than 10
         assertTrue(myNumber <=100) ;
         // all PASSED
-
-
-
     }
+    
+    //using CSV file as source for parameterized test
+    @ParameterizedTest
+    @CsvFileSource (resources = "/zipcode.csv" , numLinesToSkip = 1)
+    public void test2 (String zip) {
+        System.out.println("zip = " + zip);
+        // sending request to zipcode endpoint here
+        // api.zippopotam.us/us/90210
+        // api.zippopotam.us/us/{zipcode}
+        // baseurl : api.zippopotam.us
+        // endpoint : /us/{zipcode}
+        given()
+                .baseUri("https://api.zippopotam.us")
+                .pathParam("zipcode" , zip ).
+        when()
+                .get("/us/{zipcode}").
+        then()
+                .statusCode(is (200))
+
+                ;
+    }
+
+
+    @ParameterizedTest
+    @CsvFileSource (resources = "/country_zip.csv" , numLinesToSkip = 1)
+    public void testCountryZipCodeCombo (String csvCountry , int csvZip) {
+
+        given()
+                .log().all()
+                .baseUri("https://api.zippopotam.us")
+                .pathParam("country" , csvCountry)
+                .pathParam("zipcode" , csvZip).
+        when()
+                .get("/{country}/{zipcode}").
+        then()
+                .statusCode(200);
+
+        // ALL 7 tests works very well
+    }
+
+
+
 
 }
