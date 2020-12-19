12916d708b9a084f689c5b4d9a6db3bfbc320618
JHY34 <hamityilmaz34@gmail.com>
2020-12-18 22:35:22 -0600

    GitHubRestAPITest class is created and imports are added

---
+++ src/test/java/Z_Practice/Review_Day03/GitHubRestAPITest.java
@@ -0,0 +1,17 @@
+package Z_Practice.Review_Day03;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utility.ConfigurationReader;
+
+import static io.restassured.RestAssured.* ;
+import static io.restassured.matcher.RestAssuredMatchers.* ;
+import static org.hamcrest.Matchers.* ;
+import static org.hamcrest.MatcherAssert.assertThat ;
+
+public class GitHubRestAPITest {
+}
