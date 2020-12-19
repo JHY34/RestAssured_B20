5f1687d26eb9b4732acf0fdb6dbb5f71d155c6d0
JHY34 <hamityilmaz34@gmail.com>
2020-12-19 11:35:23 -0600

    same test with modifiying the list

--- src/test/java/day09/TestingOutLombokDependency.java
+++ src/test/java/day09/TestingOutLombokDependency.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import pojo.Department;
 import testbase.HR_ORDS_TestBase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.*;
@@ -33,6 +34,17 @@ public class TestingOutLombokDependency extends HR_ORDS_TestBase {
 
         List<Department> allDeps = get("/departments").jsonPath().getList("items", Department.class);
         allDeps.forEach(System.out::println);
+        // prints everything
+
+        // COPY THE CONTENT OF THIS LIST INTO NEW LIST
+        // AND ONLY PRINT IF THE DEP MANAGER_ID IS NOT NULL
+
+        // allDeps.removeIf(p->p.getManager_id()!=0);
+        // THIS WILL GIVE ERROR BEUCASE IT IS UNMODIFIBLE
+
+        List<Department> allDepsCopy = new ArrayList<>(allDeps);
+        allDepsCopy.removeIf(p-> p.getManager_id() == 0);
+        allDepsCopy.forEach(System.out::println);
 
     }
 
