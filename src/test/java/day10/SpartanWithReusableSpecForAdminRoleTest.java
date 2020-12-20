fb99a1a422140e39358283e77146b7c3ee5d9e1f
JHY34 <hamityilmaz34@gmail.com>
2020-12-20 13:58:21 -0600

    tested negative POST scenario

--- src/test/java/day10/SpartanWithReusableSpecForAdminRoleTest.java
+++ src/test/java/day10/SpartanWithReusableSpecForAdminRoleTest.java
@@ -19,6 +19,8 @@ public class SpartanWithReusableSpecForAdminRoleTest {
 
     static RequestSpecification givenSpec;
     static ResponseSpecification thenSpec;
+    static RequestSpecification postReqSpec ;
+    static Spartan randomSpartanPayload ;
 
     @BeforeAll
     public static void setUp(){
@@ -33,6 +35,11 @@ public class SpartanWithReusableSpecForAdminRoleTest {
         // log().all() will not work work with expect()
         // in order to make it work we need to use different method
         // logDetail(LogDetail.ALL) to provide how much we want to log
+
+        randomSpartanPayload = SpartanUtil.getRandomSpartanPOJO_Payload();
+        postReqSpec = given().spec(givenSpec)
+                .contentType(ContentType.JSON)
+                .body(randomSpartanPayload) ;
 
     }
 
@@ -116,6 +123,38 @@ public class SpartanWithReusableSpecForAdminRoleTest {
                 .time(lessThan(1L) , TimeUnit.SECONDS)
 
                 ;
+
+    }
+
+
+    @DisplayName("Test POST /api/spartans Endpoint negative scenario")
+    @Test
+    public void testBadRequest400responseBody(){
+        Spartan badPayload = new Spartan("A","A",100L) ;
+        String nameErrorMessage     = "name should be at least 2 character and max 15 character" ;
+        String genderErrorMessage   = "Gender should be either Male or Female" ;
+        String phoneErrorMessage    = "Phone number should be at least 10 digit and UNIQUE!!" ;
+        given()
+                .spec( postReqSpec )
+                .body(badPayload).
+        when()
+                .post("/spartans").
+        then()
+                .statusCode(400)
+                .body("errors" , hasSize(3))
+                .body("errors.defaultMessage" ,
+                        containsInAnyOrder(nameErrorMessage , genderErrorMessage, phoneErrorMessage))
+                .body("message" , containsString("Error count: 3"));
+
+        /*
+         verify the errors field has value of json array with 3 items
+         verify default messages for those errors :
+                "Gender should be either Male or Female"
+                "name should be at least 2 character and max 15 character"
+                "Phone number should be at least 10 digit and UNIQUE!!"
+        verify the message field contains "Error count: 3"
+         */
+
 
     }
 
