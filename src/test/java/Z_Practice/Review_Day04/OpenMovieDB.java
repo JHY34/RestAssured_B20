7c4dc0805075b91eabddea96af7f98fe4c109342
JHY34 <hamityilmaz34@gmail.com>
2020-12-21 22:53:07 -0600

    started review day 4

---
+++ src/test/java/Z_Practice/Review_Day04/OpenMovieDB.java
@@ -0,0 +1,27 @@
+package Z_Practice.Review_Day04;
+
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utility.ConfigurationReader;
+
+import java.util.List;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class OpenMovieDB {
+
+    @BeforeAll
+    public static void setUp () {
+        baseURI = "";
+        basePath = "";
+    }
+
+    @AfterAll
+    public static void tearDown () {
+        reset();
+    }
+}
