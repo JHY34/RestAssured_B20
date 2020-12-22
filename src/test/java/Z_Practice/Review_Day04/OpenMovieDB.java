578baba24208e35de99523fde86fc8bbddee5062
JHY34 <hamityilmaz34@gmail.com>
2020-12-21 23:56:16 -0600

    movie class request and response

--- src/test/java/Z_Practice/Review_Day04/OpenMovieDB.java
+++ src/test/java/Z_Practice/Review_Day04/OpenMovieDB.java
@@ -1,5 +1,6 @@
 package Z_Practice.Review_Day04;
 
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -16,12 +17,39 @@ public class OpenMovieDB {
 
     @BeforeAll
     public static void setUp () {
-        baseURI = "";
-        basePath = "";
+        baseURI = "http://www.omdbapi.com";
+        basePath = "/?";
     }
 
     @AfterAll
     public static void tearDown () {
         reset();
     }
+
+
+    @DisplayName("Get Single Movie")
+    @Test
+    public void getSingleMovie () {
+
+        // http://www.omdbapi.com/?t=Hababam
+        // apikey : 429d5914
+
+        JsonPath jp =  given()
+                .queryParam("apikey" , "429d5914")
+                .queryParam("t" , "Hababam")
+                .accept(ContentType.JSON).
+        when()
+                .get("").prettyPeek().
+        then()
+                .statusCode(is (200))
+                .contentType(ContentType.JSON).extract().jsonPath();
+
+        System.out.println("jp.getString(\"Title\") = " + jp.getString("Ratings.Value"));
+        System.out.println("jp.getString(\"Genre\") = " + jp.getJsonObject("Writer"));
+
+
+    }
+
+
+
 }
