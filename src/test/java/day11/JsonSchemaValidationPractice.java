e35478f3e68732d0ee6109ae779b03d3051670cb
JHY34 <hamityilmaz34@gmail.com>
2020-12-22 18:32:08 -0600

    2 tests are updated

--- src/test/java/day11/JsonSchemaValidationPractice.java
+++ src/test/java/day11/JsonSchemaValidationPractice.java
@@ -1,8 +1,10 @@
 package day11;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import testbase.SpartanAdminTestBase;
+import utility.SpartanUtil;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
@@ -20,6 +22,22 @@ public class JsonSchemaValidationPractice extends SpartanAdminTestBase {
                 then()
                 .assertThat()
                 .body(matchesJsonSchemaInClasspath("allSpartansSchema.json")) ;
+
+    }
+
+    @DisplayName("Testing POST /spartans endpoint response structure")
+    @Test
+    public void testPostSpartanResponseSchema(){
+
+        given()
+                .spec(adminReqSpec)
+                .contentType(ContentType.JSON)
+                .body(SpartanUtil.getRandomSpartanPOJO_Payload()).
+                when()
+                .post("/spartans").
+                then()
+                .body(matchesJsonSchemaInClasspath("postSuccessResponseSchema.json") )
+        ;
 
     }
 
