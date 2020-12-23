baf488bd714588ba6d488553c9a976af3a1565c2
JHY34 <hamityilmaz34@gmail.com>
2020-12-23 00:43:17 -0600

    posting new data

--- src/test/java/Z_Practice/Review_Day04/Spartan_POST_PUT_Tests.java
+++ src/test/java/Z_Practice/Review_Day04/Spartan_POST_PUT_Tests.java
@@ -1,5 +1,6 @@
 package Z_Practice.Review_Day04;
 
+import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.AfterAll;
@@ -15,6 +16,51 @@ import static org.hamcrest.Matchers.*;
 
 public class Spartan_POST_PUT_Tests {
 
-    @DisplayName("POST Test")
+    @BeforeAll
+    public static void setUp() {
+        baseURI = ConfigurationReader.getProperty("spartan.base_url");
+        basePath = "/api";
+    }
+
+    @DisplayName("POST A Spartan")
+    @Test
+    public void addOneSpartan () {
+
+        Faker faker = new Faker();
+
+//        String jsonObject = " {\n" +
+//                "            \"name\" : faker.funnyName().name() ,\n" +
+//                "            \"gender\" : faker.demographic().sex() ,\n" +
+//                "            \"phone\" : faker.number().numberBetween(5000000000L, 999999999L)\n" +
+//                "        } " ;
+
+
+        String name = "Kerem";
+        String gender = "Male";
+        long phone = 1234561234L;
+
+
+
+        //JsonPath  jp =
+                given()
+                                    .auth().basic("admin" , "admin")
+                                    .log().all()
+                                    .contentType(ContentType.JSON)
+                                    .body("name" )
+                                    .
+                            when()
+                                    .get("/spartans").
+                            then()
+                                    .log().all()
+                                    .statusCode(is(200))
+                                    .contentType(ContentType.JSON)
+                                    .extract().jsonPath()
+
+                                    ;
+
+      //  System.out.println("jp.getJsonObject(\"Body\") = " + jp.getJsonObject("Body"));
+
+
+    }
 
 }
