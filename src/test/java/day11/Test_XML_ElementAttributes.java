3fc84520ff4e826d634e6058abf26eb6e257e3b6
JHY34 <hamityilmaz34@gmail.com>
2020-12-22 20:17:07 -0600

    new class is created

---
+++ src/test/java/day11/Test_XML_ElementAttributes.java
@@ -0,0 +1,14 @@
+package day11;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class Test_XML_ElementAttributes {
+    @DisplayName("Test omdbapi xml response and movie info")
+    @Test
+    public void testMovieAttributes(){
+        // search for your favorite movie
+        // assert the movie info according to your expected result
+    }
+}
