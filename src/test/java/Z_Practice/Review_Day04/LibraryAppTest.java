bbccc29c53edb4264aa4bddf139eb90141227e92
JHY34 <hamityilmaz34@gmail.com>
2020-12-24 22:10:15 -0600

    git commit -m" Started the new library app test and added imports and annotations "

---
+++ src/test/java/Z_Practice/Review_Day04/LibraryAppTest.java
@@ -0,0 +1,36 @@
+package Z_Practice.Review_Day04;
+
+import com.github.javafaker.Faker;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utility.ConfigurationReader;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class LibraryAppTest {
+
+    @BeforeAll
+    public static void setUp() {
+        baseURI = ConfigurationReader.getProperty("spartan.base_url");
+        basePath = "/api";
+    }
+
+    @AfterAll
+    public static void tearDown () {
+        reset();
+    }
+
+
+
+}
