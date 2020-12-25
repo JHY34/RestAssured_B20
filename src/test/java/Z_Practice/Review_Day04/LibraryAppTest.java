515b2d2445b8233400bc28398f0355e0a4eb2798
JHY34 <hamityilmaz34@gmail.com>
2020-12-24 23:21:25 -0600

    git commit -m"  the library test login"

--- src/test/java/Z_Practice/Review_Day04/LibraryAppTest.java
+++ src/test/java/Z_Practice/Review_Day04/LibraryAppTest.java
@@ -43,7 +43,7 @@ public class LibraryAppTest {
         password --> KNPXrm3S
          */
 
-        given()
+        JsonPath jp = given()
                 .log().all()
                 .contentType(ContentType.URLENC)
                 .formParam("email" ,"librarian69@library" )
@@ -54,9 +54,11 @@ public class LibraryAppTest {
                 .log().all()
                 .statusCode(200)
                 .body(is(not(emptyString())))
-                .body("token" , not(emptyString()))
-
+                .body("token" , not(emptyString())).extract().jsonPath()
                 ;
+
+        System.out.println("token = " + jp.getString("token"));
+
 
 
     }
