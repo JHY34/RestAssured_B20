08f7ee47d82c65e28be8844cd94a7a1d9a7d2b02
JHY34 <hamityilmaz34@gmail.com>
2020-12-26 00:16:00 -0600

    git commit -m" matcher class is created and some matchers are used to practice "

---
+++ src/test/java/Z_Practice/Review_Day05/Hamcrest_Collection_Support.java
@@ -0,0 +1,29 @@
+package Z_Practice.Review_Day05;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class Hamcrest_Collection_Support {
+
+    @Test
+    public void assertionTests () {
+
+        List<Integer> listOfNumbers = Arrays.asList(4,6,8,12,34,56,90);
+
+        assertThat (listOfNumbers, hasSize (7));
+
+        assertThat(listOfNumbers , hasItems(4,6,8));
+
+        assertThat(listOfNumbers , not (hasItem(40)));
+
+    }
+
+
+
+
+}
