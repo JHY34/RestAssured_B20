230d0f5089465e990237edb0b0d2910c0408475d
JHY34 <hamityilmaz34@gmail.com>
2020-12-25 22:33:50 -0600

    git commit -m"created a new class to extract ata from the response "

---
+++ src/test/java/Z_Practice/Review_Day05/Extract_Practice.java
@@ -0,0 +1,24 @@
+package Z_Practice.Review_Day05;
+
+import com.github.javafaker.Faker;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.*;
+import utility.ConfigurationReader;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class Extract_Practice {
+
+
+
+}
+
+
