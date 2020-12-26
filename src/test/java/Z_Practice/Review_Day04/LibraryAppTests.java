294231fecaea7276073bde938388ca2d6826a1c1
JHY34 <hamityilmaz34@gmail.com>
2020-12-25 20:39:50 -0600

    git commit -m"new practice test is created for the libray application"

---
+++ src/test/java/Z_Practice/Review_Day04/LibraryAppTests.java
@@ -0,0 +1,35 @@
+package Z_Practice.Review_Day04;
+
+import com.github.javafaker.Faker;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.*;
+import utility.ConfigurationReader;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class LibraryAppTests {
+
+    @BeforeAll
+    public static void setUp() {
+        baseURI = "http://library1.cybertekschool.com";
+        basePath = "/rest/v1";
+    }
+
+
+    @AfterAll
+    public static void tearDown () {
+        reset();
+    }
+
+
+
+
+}
