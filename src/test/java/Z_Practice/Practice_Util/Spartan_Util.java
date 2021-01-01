570304cf3e5d07243b2afe9b65672eddfa8d0cb1
JHY34 <hamityilmaz34@gmail.com>
2021-01-01 00:13:44 -0600

    get 1 spartan payload

--- src/test/java/Z_Practice/Practice_Util/Spartan_Util.java
+++ src/test/java/Z_Practice/Practice_Util/Spartan_Util.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.*;
 import utility.ConfigurationReader;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,47 +18,34 @@ import static org.hamcrest.Matchers.*;
 
 public class Spartan_Util {
 
-    @BeforeAll
-    public static void setUp() {
-        baseURI = ConfigurationReader.getProperty("spartan.base_url");
-        basePath = "/api";
-    }
-
-    @AfterAll
-    public static void tearDown () {
-        reset();
-    }
-
-
-    @DisplayName("Create 1 Spartan")
-    @Test
-    public void createOneSpartan () {
+//    @BeforeAll
+//    public static void setUp() {
+//        baseURI = ConfigurationReader.getProperty("spartan.base_url");
+//        basePath = "/api";
+//    }
+//
+//    @AfterAll
+//    public static void tearDown() {
+//        reset();
+//    }
+//
+//
+//    @DisplayName("Get 1 Spartan Payload")
+//    @Test
+    public static Map<String, Object> getOneSpartanPayload() {
 
         Faker faker = new Faker();
 
-        String randomSpartan = "{\n" +
-                "            \"name\" : faker.funnyName().name();\n" +
-                "            \"gender\" : faker.demographic().sex();\n" +
-                "            \"phone\" : faker.number().numberBetween(3_000_000_000L , 9_999_999_999L);\n" +
-                "        }";
+        Map<String, Object> spartanPayload = new LinkedHashMap<>();
+        spartanPayload.put("name", faker.name().firstName());
+        spartanPayload.put("gender", faker.demographic().sex());
+        spartanPayload.put("phone", faker.number().numberBetween(3_000_000_000L, 9_999_999_999L));
 
-//        {
-//            "name" : faker.funnyName().name();
-//            "gender" : faker.demographic().sex();
-//            "phone" : faker.number().numberBetween(3_000_000_000L , 9_999_999_999L);
-//        }
+        return spartanPayload;
 
-
-        given()
-                .auth().basic("admin", "admin")
-                .contentType(ContentType.JSON)
-                .body(randomSpartan).
-        when()
-                .post("/spartans").
-        then()
-                .statusCode(201)
-                .contentType(ContentType.JSON);
     }
 
-
+    public static void main(String[] args) {
+        System.out.println(getOneSpartanPayload());
+    }
 }
