0e83c102cb5b91ee10c77d6bf350791cd0789704
JHY34 <hamityilmaz34@gmail.com>
2021-01-03 21:50:02 -0600

    Spartan POJO class with 4 fields created

---
+++ src/test/java/Z_Practice/Review_Day06/SpartanRead.java
@@ -0,0 +1,7 @@
+package Z_Practice.Review_Day06;
+
+// This is a POJO class
+// used to represent Spartan Data in Response Json
+// it has 4 fields: id, name, gender, phone:
+public class SpartanRead {
+}
